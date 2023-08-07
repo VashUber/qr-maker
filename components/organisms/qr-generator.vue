6ec37c566299a37212974d0ad7f25bccb885ce3c
VashUber <71492611+VashUber@users.noreply.github.com>
2023-08-07 14:37:20 +0300

    feat: hide save functional for guests

--- components/organisms/qr-generator.vue
+++ components/organisms/qr-generator.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <div class="flex flex-col items-center gap-2">
+      <h1 class="text-3xl">Generate your qr code</h1>
       <div class="h-72 w-72 bg-white">
         <atom-canvas ref="canvasRef" class="!h-72 !w-72" />
       </div>
-      <atom-input v-model.trim="qrSource" />
-      <atom-input v-model.trim="qrTitle" />
-      <atom-button :disabled="isDisable" @click="save">Save</atom-button>
+      <atom-input v-model.trim="qrSource" placeholder="Enter QR source" />
+      <template v-if="data?.user">
+        <atom-input v-model.trim="qrTitle" placeholder="Enter Title" />
+        <atom-button :disabled="isDisable" @click="save">Save</atom-button>
+      </template>
     </div>
   </div>
 </template>
@@ -15,6 +18,7 @@
 import { toDataURL } from 'qrcode'
 import { watchDebounced, watchOnce } from '@vueuse/core'
 
+const { data } = useAuth()
 const canvasRef = ref<{ elRef: HTMLCanvasElement }>(null!)
 const qrSource = ref('')
 const qrTitle = ref('')
@@ -22,6 +26,10 @@ const isDisable = ref(false)
 let dataUrl = ''
 
 const save = async () => {
+  if (dataUrl.length === 0 || qrTitle.value.length === 0) {
+    return
+  }
+
   await useFetch('/api/save-qr', {
     method: 'POST',
     body: {
